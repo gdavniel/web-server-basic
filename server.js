c74dcaff0f8bee04dfa9b6f8d2ae766b8dac9c0a
gdavniel <gdel.daniel@gmail.com>
2020-01-05 10:03:37 -0500

    servir contenido estatico usando middleware

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  
-  let salida = {
-    nombre: 'daniel',
-    edad: 24,
-    url: req.url
-  }
+// middlewares, un callback que se ejcuta siempre, 
+// para cualquier url que el usuario pida
 
-  // JSON, send detecta el objeto
-  res.send(salida)
-})
+// dominio publico, para acceder a archivos se pone la extension,
+// sino buscara un servicio
+app.use(express.static(__dirname + '/public'))
+
+// app.get('/', (req, res) => {
+  
+//   let salida = {
+//     nombre: 'daniel',
+//     edad: 24,
+//     url: req.url
+//   }
+
+//   // JSON, send detecta el objeto
+//   res.send(salida)
+// })
 
 app.listen(3000)
