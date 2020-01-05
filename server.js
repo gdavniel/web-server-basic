eeddd6664d24bdc1f9298e2101daf27caf0b628b
gdavniel <gdel.daniel@gmail.com>
2020-01-05 14:33:30 -0500

    usando handdlebars, registerPartials y helpers

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+var hbs = require('hbs');
+ 
 // middlewares, un callback que se ejcuta siempre, 
 // para cualquier url que el usuario pida
 
@@ -8,7 +10,15 @@ const app = express()
 // sino buscara un servicio
 app.use(express.static(__dirname + '/public'))
 
-// app.get('/', (req, res) => {
+// HBS engine
+
+hbs.registerPartials(__dirname + '/views/partials'); // como componentes
+app.set('view engine', 'hbs')
+
+// Helpers
+require('./hbs/helpers')
+
+app.get('/', (req, res) => {
   
 //   let salida = {
 //     nombre: 'daniel',
@@ -18,6 +28,28 @@ app.use(express.static(__dirname + '/public'))
 
 //   // JSON, send detecta el objeto
 //   res.send(salida)
-// })
+
+    res.render('home', {
+        nombre: 'daNiel',
+        // anio: new Date().getFullYear() // si no existe la variable en los componentes busca en los helpers
+    })
+})
+
+app.get('/about', (req, res) => {
+  
+    //   let salida = {
+    //     nombre: 'daniel',
+    //     edad: 24,
+    //     url: req.url
+    //   }
+    
+    //   // JSON, send detecta el objeto
+    //   res.send(salida)
+    
+        res.render('about', {
+            // anio: new Date().getFullYear()
+        })
+    })
+    
 
 app.listen(3000)
